1086a98a1dddb49411a7f1c1ef8787864460fc07
Brad Carlson <b2carlso@yahoo.com>
2018-04-22 00:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name){
   return drivers.push(name);
 }
+
+function destructivelyPrependDriver(name){
+  return drivers.shift(name);
+}
